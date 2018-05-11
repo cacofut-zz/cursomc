b8d1eaeccec6efbe41a460ae3ec31537dec05ed6
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2018-05-11 20:57:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/diagnosticit/CursomcApplication.java
@@ -0,0 +1,12 @@
+package br.com.diagnosticit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+//d fsdfsd 
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
