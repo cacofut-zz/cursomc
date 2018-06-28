a30548df206b5c94f8f7a5686d1365bec9745b24
Cristiano <cacofut@hotmail.com>
2018-06-28 02:13:07 -0300

    Insert Categoria nova

--- src/main/java/br/com/diagnosticit/resources/CategoriaResource.java
+++ src/main/java/br/com/diagnosticit/resources/CategoriaResource.java
@@ -8,8 +8,6 @@ package br.com.diagnosticit.resources;
 import br.com.diagnosticit.resources.domain.Categoria;
 import br.com.diagnosticit.resources.services.CategoriaService;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +22,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  * @author cristianoca
  */
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value="/categorias" )
 public class CategoriaResource {
     
     @Autowired
